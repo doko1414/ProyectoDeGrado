5650ec45074bfd74b4d12e6b4d0517ffb35a0190
javier <javierlarasolis1414@gmail.com>
2024-10-07 17:28:27 -0400

    documento excel  corregido

--- PersonalizacionProyectoGradoWASM/wwwroot/js/common.js
+++ PersonalizacionProyectoGradoWASM/wwwroot/js/common.js
@@ -91,7 +91,7 @@ function crearGraficoPedidos(pendientes, enProceso, enviados, entregados, cancel
 function saveAsFile(filename, bytesBase64) {
     var link = document.createElement('a');
     link.download = filename;
-    link.href = "data:application/octet-stream;base64," + bytesBase64;
+    link.href = "data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64," + bytesBase64;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
